040e4a2fae9c9eb5fd0dce4bd6f2d52e713d985e
Artur Buss <buss.art7@gmail.com>
2020-06-19 13:21:02 +0600

    adapter refactoring

--- app/src/main/java/com/grechka/krvmfm/adapters/SpinsListAdapter.kt
+++ app/src/main/java/com/grechka/krvmfm/adapters/SpinsListAdapter.kt
@@ -1,31 +1,53 @@
 package com.grechka.krvmfm.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.grechka.krvmfm.R
-import com.grechka.krvmfm.ViewHolder
 import com.grechka.krvmfm.objects.Spin
-import kotlin.reflect.KMutableProperty0
+import kotlin.reflect.KProperty0
 
-class SpinsListAdapter(private val items: KMutableProperty0<ArrayList<String>>) :
-    RecyclerView.Adapter<ViewHolder>() {
+class SpinsListAdapter(private val items: KProperty0<ArrayList<String>>) :
+    RecyclerView.Adapter<SpinsListAdapter.ViewHolder>() {
 
     private val gson = Gson()
 
     override fun getItemCount() = items.get().size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        LayoutInflater.from(parent.context)
             .inflate(R.layout.queue_item, parent, false)
-        return ViewHolder(itemView)
-    }
+    )
+
 
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
         val spin = gson.fromJson(items.get()[i], Spin::class.java)
         holder.bind(spin)
-        Log.d("onBind",spin.a)
+    }
+
+    inner class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        var textViewArtist: TextView? = null
+        var textViewSong: TextView? = null
+        var textViewAlbum: TextView? = null
+        var textViewTime: TextView? = null
+        var textViewImg: TextView? = null
+
+        init {
+            textViewArtist = itemView?.findViewById(R.id.item_artist)
+            textViewSong = itemView?.findViewById(R.id.item_song)
+            textViewAlbum = itemView?.findViewById(R.id.item_album)
+            textViewTime = itemView?.findViewById(R.id.item_time)
+        }
+
+        fun bind(spin: Spin) {
+            textViewArtist?.text = spin.a
+            textViewSong?.text = spin.s
+            textViewAlbum?.text = spin.r
+            textViewTime?.text = spin.t
+            textViewImg?.text = spin.img
+        }
     }
 }
